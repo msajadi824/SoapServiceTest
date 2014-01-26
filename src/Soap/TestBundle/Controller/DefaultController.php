3ef83bcbf6969c9758390ce6104c1b4590e87751
msajadi824 <msajadi824@hotmail.com>
2014-01-26 20:06:08 +0330

    aaaa

--- src/Soap/TestBundle/Controller/DefaultController.php
+++ src/Soap/TestBundle/Controller/DefaultController.php
@@ -3,10 +3,16 @@
 namespace Soap\TestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller {
     public function indexAction()
     {
         return $this->render("SoapTestBundle:Default:index.html.twig");
+    }
+
+    public function getNameByIdAction($id)
+    {
+        return new Response('aa');
     }
 } 
